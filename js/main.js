fe9e808a61b5e240b4ee8b29c2810f62937c3b91
broncs <jhowfrombrazil@gmail.com>
2021-04-10 13:13:46 -0300

    fixing details home page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 import { a } from './modules/testing.js';
 
 console.log(a);
+
+const loginButton = document.querySelector('.btn-primary');
+
+loginButton.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  window.location.href = 'home.html';
+});
